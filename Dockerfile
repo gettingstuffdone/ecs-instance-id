3577126fd4320aefac9551696ba54c2e3c2d7b54
gettingstuffdone <17427785+gettingstuffdone@users.noreply.github.com>
2017-11-04 23:54:11 +1100

    fix bootstrap.sh add descriptive text to index.html

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM nginx:alpine
-EXPOSE 8080
-COPY . /usr/share/nginx/html
-CMD ["nginx", "-g", "daemon off;"]
+
+RUN apk update \
+ && apk add --update jq \
+ && apk add --update curl \
+ && rm -rf /var/cache/apk/*
+
+COPY index.html /usr/share/nginx/html
+COPY bootstrap.sh /tmp/bootstrap.sh
+
+CMD ["sh", "/tmp/bootstrap.sh"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
-RESULT=$(curl http://localhost:51678/v1/metadata)
-sed -i "s/Heading/$RESULT/g" index.html
+RESULT=$(curl http://172.17.0.1:51678/v1/metadata | jq ."ContainerInstanceArn" -r | awk -F/ '{print $NF}' )
+echo $(curl http://172.17.0.1:51678/v1/metadata)
+echo before result
+echo $RESULT
+echo after result
+sed -i "s/Heading/$RESULT/g" /usr/share/nginx/html/index.html
+nginx -g 'daemon off;'
